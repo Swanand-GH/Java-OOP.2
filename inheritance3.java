3e65f0929501c4aeb883d00d341aa4aaffc3f7f5
Swanand-GH <swanandb.ml25@sbjit.edu.in>
2026-08-22 20:03:49 +0530

    Add inheritance3 - OOP inheritance demo (Student, CollegeStudent)

---
+++ inheritance3.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class Student {
+    String name;
+
+    Student(String name) {
+        this.name = name;
+    }
+}
+
+class CollegeStudent extends Student {
+    String course;
+
+    CollegeStudent(String name, String course) {
+        super(name);   // Calls parent class constructor
+        this.course = course;
+    }
+
+    void display() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Course: " + course);
+    }
+}
+
+public class inheritance3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        String course = sc.nextLine();
+
+        CollegeStudent student = new CollegeStudent(name, course);
+
+        student.display();
+    }
+}
